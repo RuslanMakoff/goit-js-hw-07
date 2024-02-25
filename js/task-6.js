c184b420b5b6f4e87bbee685779bf570fe97ce5c
Ruslan Makovetskyi <146139235+RuslanMakoff@users.noreply.github.com>
2024-02-25 21:13:29 +0100

    add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  const boxesContainer = document.getElementById('boxes');
+
+  createBtn.addEventListener('click', createBoxes);
+  destroyBtn.addEventListener('click', destroyBoxes);
+
+  function createBoxes() {
+    const amount = input.valueAsNumber;
+    if (amount < 1 || amount > 100 || isNaN(amount)) {
+      alert('Please enter a number between 1 and 100.');
+      return;
+    }
+
+    boxesContainer.innerHTML = '';
+
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+      size += 10;
+    }
+
+    input.value = '';
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
